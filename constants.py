33234fd4ec08fe2fadc283bae5464aebdead8a39
marzookcyril <cyrilmarzook@gmail.com>
2024-01-02 11:58:23 +0100

    code added

---
+++ constants.py
@@ -0,0 +1,15 @@
+
+VOCAB_SIZE = 169717
+MAX_LENGTH = 400
+LABELS = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
+EMBEDDINGS = {
+    "GloVe 6B 50D": "glove.6B.50d.txt",
+    "GloVe 6B 100D": "glove.6B.100d.txt",
+    "GloVe 6B 200D": "glove.6B.200d.txt",
+    "GloVe 6B 300D": "glove.6B.300d.txt",
+}
+
+
+
+
+
